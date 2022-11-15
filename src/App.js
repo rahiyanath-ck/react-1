253dd1311a3407d3fb17586620afa5f5ca355f33
rahiyanath-ck <rahiyanathedavanna@gmail.com>
2022-11-15 14:16:19 +0530

    first

--- src/App.js
+++ src/App.js
@@ -5,18 +5,47 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>Hello world !!!!!</h1>
+      <p>welcome to india</p>
+      <img src="https://www.floraqueen.com/blog/wp-content/uploads/2020/02/shutterstock_552296878.jpg" alt="" />
+      <a href="https://www.thoughtco.com/thmb/EDs7G-rWTFO8YsElJdHUMHVCdzY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/lotus-flower-828457262-5c6334b646e0fb0001dcd75a.jpg">click here</a>
+      <h2><b>hello students..</b></h2>
+      <h2><i>how are you</i></h2>
+      <h2><u>hello dear children..</u></h2>
+      <ul>
+        <li>malappuram</li>
+        <li>idukki</li>
+        <li>kollam</li>
+      </ul>
+     <ol>
+      <li>mango</li>
+      <li>apple</li>
+      <li>pappaya</li>
+     </ol>
+     <div class="tab">
+     <table>
+      <tr>
+        <th>Name</th>
+        <th>class</th>
+        <th>Place</th>
+      </tr>
+      <tr>
+        <td>raj</td>
+        <td>8</td>
+        <td>malappuram</td>
+      </tr>
+      <tr>
+        <td>ram</td>
+        <td>6</td>
+        <td>kollam</td>
+      </tr>
+      <tr>
+        <td>kiran</td>
+        <td>7</td>
+        <td>kannur</td>
+      </tr>
+     </table>
+     </div>
       </header>
     </div>
   );
